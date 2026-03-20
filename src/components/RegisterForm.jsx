11b9fa3bde29059c2cb8b4b0b7dc95ee2494c9ec
Charlielifts <benchbautis@gmail.com>
2026-03-21 00:35:31 +0800

    Added new frontend components

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,4 +1,40 @@
-const Registerform = () => {
- 
+
+const Registerform = ({active}) => {
+ return ( 
+    <div className={`absolute left-0 w-1/2 h-full flex items-center text-center p-15" ${active ? "translate-x-0 opacity-100" : "-translate-x-full opacity-0"}`}>
+        <form className="w-full">
+                <h1 className="text-3xl font-bold mb-6 cursor-default">Register<i class="fa-solid fa-align-center"></i></h1>
+
+            <div>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    className=" w-full py-3 pl-5 pr-10 bg-gray-300 rounded-full outline-none"
+                />
+            </div>
+
+            <div className="relative my-6">
+                <input
+                    type="email"
+                    placeholder="Email"
+                    className=" w-full py-3 pl-5 pr-10 bg-gray-300 rounded-full outline-none"
+
+                />
+            </div>
+            
+            <div className="relative my-6">
+                <input 
+                    type="password"
+                    placeholder="Password"
+                    className=" w-full py-3 pl-5 pr-10 bg-gray-300 rounded-full outline-none" 
+                />
+            </div>
+
+            <button className=" w-full py-3 pl-5 pr-10 bg-amber-500 rounded-full font-semibold hover:bg-amber-600 hover:scale-105 transition duration-300 cursor-pointer">
+                Register
+            </button>
+        </form>
+    </div>
+ )
 }
 export default Registerform
--- src/components/togglepanel.jsx
+++ src/components/togglepanel.jsx
@@ -1,5 +1,43 @@
-const TogglePanel = ({setActive}) => {
 
+const TogglePanel = ({ active, setActive }) => {
+    return(
+        <div className="absolute w-full h-full">
+            <div className={`absolute left-0 w-1/2 h-full flex flex-col justify-center items-center text-white
+                bg-gradient-to-r from bg-red-500  to-amber-400 rounded-r-[38%] cursor-default 
+                transition-all duration-700 
+                ${active ? "-translate-x-full" : "translate-x-0"}`}>
+                
+                <h1 className="text-3xl font-pacifico mb-4 cursor-default">
+                Hello, Welcome<i class="fa-solid fa-door-open"></i>
+                </h1>
+                <p className="text-center font-Gummy mb-4">
+                    Already have an account?
+                </p>
+
+                <button onClick={() => setActive(true)} className=" border border-white hover:bg-transparent hover:scale-105 transtion duration-100 py-2 px-6 w-60 rounded-full cursor-pointer"> 
+                    Register
+                </button>
+            </div>
+
+            <div className={`absolute right-0 w-1/2 h-full flex flex-col justify-center items-center text-white
+                bg-gradient-to-l from bg-amber-400  to-red-500 rounded-l-[38%] cursor-default 
+                transition-all duration-700 
+                ${active ? "translate-x-0" : "translate-x-full"}`}>
+                
+                <h1 className="text-3xl font-pacifico mb-4 cursor-default">
+                Welcome Back<i class="fa-jelly fa-regular fa-heart"></i>
+                </h1>
+                <p className="text-center font-Gummy mb-4">
+                    Already have an account?
+                </p>
+
+                <button onClick={() => setActive(false)} className=" border border-white hover:bg-transparent hover:scale-105 transtion duration-100 py-2 px-6 w-60 rounded-full cursor-pointer"> 
+                    Log in
+                </button>
+            </div>
+
+        </div>   
+    )  
 
 }
 export default TogglePanel
